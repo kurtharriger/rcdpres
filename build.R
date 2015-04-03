20f0bc522fbb64eb50fbb24d252f85c0cd122dc1
Kurt Harriger <kharriger@rallydev.com>
2015-04-03 10:12:04 -0600

    publish to rpub

--- build.R
+++ build.R
@@ -1,3 +1,4 @@
 #! /usr/bin/env Rscript 
 require(rmarkdown)
 rmarkdown::render('slides.Rmd', output_dir="target")
+markdown::rpubsUpload('Test Presentation', 'target/slides.html', id= "https://api.rpubs.com/api/v1/document/71314/69aafc0ec66248f193baa5197744e5b2")
